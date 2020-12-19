d3ee35c7a4d0729ab32e49b036b3df50409ee0ba
Shotaro Gotanda <g.sho1500@gmail.com>
2020-12-19 16:52:08 +0900

    refactoring log initialization
    Signed-off-by: Shotaro Gotanda <g.sho1500@gmail.com>

--- debug.c
+++ debug.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,27 +7,54 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pwd.h>
+#include "utils.h"
+
 
 FILE* logfp = NULL;
 
 void init_log(void) {
-  char cwd[PATH_MAX] = "";
-  if (getcwd(cwd, sizeof(cwd)) == NULL) {
-    perror("getcwd() error");
-    exit(1);
-  }
-  // cwd == $HOME/.local/share/containers/storage/overlay
-  strcat(cwd, "-logs");
-  mkdir(cwd, 0755);
-  strcat(cwd, "/fuse-overlayfs.log");
+  int ret;
+  cleanup_free char *log_path = NULL;
+  const char* env_path = getenv("FUSE_OVERLAY_LOG_PATH");
+  if (env_path == NULL)
+    {
+      const char *homedir = getenv("HOME");
+      if (homedir == NULL) {
+        perror("init log: homedir");
+        exit(1);
+      }
+      ret = asprintf(&log_path, "%s/fuse-overlayfs.log", homedir);
+      if (ret < 0) {
+        perror("init log: opening file");
+        exit(1);
+      }
+    }
+  else
+    {
+      log_path = strdup(env_path);
+    }
 
-  if (logfp == NULL) {
-    logfp = fopen(cwd, "a");
-  }
-  setbuf(logfp, NULL);
+  ret = access(log_path, W_OK);
+  if (ret < 0 && errno != ENOENT)
+    {
+      perror("init log: access to log path");
+      exit(1);
+    }
 
-  if (logfp == NULL) {
-    perror("init log");
-    exit(1);
-  }
+  if (logfp == NULL)
+    {
+      logfp = fopen(log_path, "a");
+      if (logfp == NULL)
+        {
+          perror("init log: opening file");
+          exit(1);
+        }
+      setbuf(logfp, NULL);
+    }
+  else
+    {
+      fprintf(stderr, "log  file is already set\n");
+          exit(1);
+    }
 }
